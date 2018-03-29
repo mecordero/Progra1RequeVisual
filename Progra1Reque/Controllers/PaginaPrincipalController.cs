3ecfd310c49aed34fc89929af7bb425469739552
Meli Cordero <melisacorderoarias@hotmail.com>
2018-03-28 19:13:30 -0600

    2 pantallas de interfaz

---
+++ Progra1Reque/Controllers/PaginaPrincipalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Progra1Reque.Controllers
+{
+    public class PaginaPrincipalController : Controller
+    {
+        // GET: PaginaPrincipal
+        public ActionResult PrincipalCliente()
+        {
+            return View();
+        }
+    }
+}
---
+++ Progra1Reque/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Progra1Reque.MvcApplication" Language="C#" %>
