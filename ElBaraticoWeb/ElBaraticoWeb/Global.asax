d882bae46d3e1cfe188f8e97ee8bb0362819898f
FA250 <faph.2009@hotmail.com>
2018-04-01 16:06:41 -0600

    Add files via upload

---
+++ ElBaraticoWeb/ElBaraticoWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElBaraticoWeb.MvcApplication" Language="C#" %>
---
+++ ElBaraticoWeb/ElBaraticoWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElBaraticoWeb.Startup))]
+namespace ElBaraticoWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
