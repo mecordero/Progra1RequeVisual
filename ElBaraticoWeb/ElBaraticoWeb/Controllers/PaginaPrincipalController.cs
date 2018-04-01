f067f6ce7b90476f413dbcfaad44b4aad4d3c38c
FA250 <faph.2009@hotmail.com>
2018-03-31 21:16:20 -0600

    Login
    Se cambio el diseño y funciona el login (con los procedimientos a la BD)
    Importante: Se debe cambiar el servidor en la conexion en el archivo web.config

---
+++ ElBaraticoWeb/ElBaraticoWeb/Controllers/PaginaPrincipalController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Progra1Reque.Controllers
+{
+    public class PaginaPrincipalController : Controller
+    {
+        // GET: PaginaPrincipalCliente
+        public ActionResult PrincipalCliente()
+        {
+            if (Session["cliente"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            return View();
+        }
+        // GET: PaginaPrincipalAdministrador
+        public ActionResult PrincipalAdministrador()
+        {
+            if (Session["cliente"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
---
+++ ElBaraticoWeb/ElBaraticoWeb/Controllers/PaginasExtraAdministradorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Progra1Reque.Controllers
+{
+    public class PaginasExtraAdministradorController : Controller
+    {
+        // GET: PaginasExtraAdministrador
+        public ActionResult CrearAdministrador()
+        {
+            return View();
+        }
+    }
+}
