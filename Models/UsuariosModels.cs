47e8c3bd95da34b8c0a400d54af2b64cd77b761b
FA250 <faph.2009@hotmail.com>
2018-04-01 16:04:01 -0600

    Add files via upload

---
+++ Models/UsuariosModels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace ElBaraticoWeb.Models
+{    public class LogInModel
+    {
+        public String usuario { get; set; }
+        public String clave { get; set; }
+        public int tipo { get; set; }
+        public int idDuenno { get; set; }
+    }
+
+    public class AdministradorModel
+    {
+        List<AdministradorModel> listaAdministradores = new List<AdministradorModel>();
+
+        public int Id { get; set; }
+        public String nombre { get; set; }
+        public String apellidos { get; set; }
+    }
+
+    public class ClienteModel
+    {
+        List<ClienteModel> listaCliente = new List<ClienteModel>();
+        
+        public int Id { get; set; }
+        public String nombre { get; set; }
+        public String apellidos { get; set; }
+        public String correoElectronico { get; set; }
+        public String direccion { get; set; }
+
+    }
+}
